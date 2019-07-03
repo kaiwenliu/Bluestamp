eaf9b480f02b109dbec02d819dcb73bbab9d50c6
Kevin Chu <kevinlc221@gmail.com>
2019-07-03 15:41:20 -0700

    Update POV_ActualCode.ino

--- POV_ActualCode/POV_ActualCode.ino
+++ POV_ActualCode/POV_ActualCode.ino
@@ -1,4 +1,4 @@
-int timer = 700;    
+int timer = 500;
 int U18 = 7;
 int State = 0;         
 int lastState = 0;     
@@ -31,73 +31,110 @@ boolean V[]={0,1,1,1,1,1,1,0,0,0,0,1,1,1,1};
 boolean X[]={1,1,0,1,1,0,0,1,0,0,1,1,0,1,1};
 boolean Y[]={0,0,1,1,1,1,1,1,0,0,0,0,1,1,1};
 boolean Z[]={1,1,0,0,1,1,0,1,0,1,1,0,0,1,1};
-boolean exclamationmark[]={1,0,1,1,1,1,0,1,1,1};
+boolean exclamationmark[]={1,0,1,1,1};
 int m;
 
-void setup() {
-  for (int Pin = 2; Pin <=6; Pin++) {
-    pinMode(Pin, OUTPUT);}
-  pinMode(U18, INPUT);
+void setup()
+{
+  for (int Pin = 2; Pin <=6; Pin++)
+  {
+    pinMode(Pin, OUTPUT);
+  }
+
+  pinMode(7, OUTPUT);
+  digitalWrite(7,LOW);
 }
 
-void loop() {
-State = digitalRead(U18);
-if (State != lastState){
-  if (State == HIGH){
-WriteLetter3Width(K);
-WriteLetter1Width(space);
-WriteLetter3Width(E);
-WriteLetter1Width(space);
-WriteLetter3Width(V);
-WriteLetter1Width(space);
-WriteLetter3Width(I);
-WriteLetter1Width(space);
-WriteLetter4Width(N);
-WriteLetter1Width(space);
-WriteLetter3Width(S);
-WriteLetter1Width(space);
-WriteLetter3Width(P);
-WriteLetter1Width(space);
-WriteLetter3Width(R);
-WriteLetter1Width(space);
-WriteLetter3Width(O);
-WriteLetter1Width(space);
-WriteLetter3Width(J);
-WriteLetter1Width(space);
-WriteLetter2Width(exclamationmark);
-WriteLetter1Width(space);}}
-lastState = State;
+void loop()
+{
+  // State = digitalRead(U18);
+
+  // if (State != lastState)
+  // {
+  //   if (State == HIGH)
+  //   {
+  WriteLetter3Width(K);
+  WriteLetter1Width(space);
+  WriteLetter3Width(E);
+  WriteLetter1Width(space);
+  WriteLetter3Width(V);
+  WriteLetter1Width(space);
+  WriteLetter3Width(I);
+  WriteLetter1Width(space);
+  WriteLetter4Width(N);
+  WriteLetter1Width(space);
+  WriteLetter3Width(S);
+  WriteLetter1Width(space);
+  WriteLetter3Width(P);
+  WriteLetter1Width(space);
+  WriteLetter3Width(R);
+  WriteLetter1Width(space);
+  WriteLetter3Width(O);
+  WriteLetter1Width(space);
+  WriteLetter3Width(J);
+  WriteLetter1Width(space);
+  WriteLetter1Width(exclamationmark);
+  WriteLetter1Width(space);
+  //   }
+  // }
+  // lastState = State;
 }
 
-void WriteLetter4Width(boolean letter[]){
+void WriteLetter4Width(boolean letter[])
+{
   m=0;
   // 4 * 5 letter
-  for (int n=0; n<=3; n++){
-  for (int Pin = 2; Pin <=6; Pin++) {
-    digitalWrite(Pin, letter[m]);
-    m=m+1;}
-    delayMicroseconds(timer);}}
+  for (int n=0; n<=3; n++)
+  {
+    for (int Pin = 2; Pin <=6; Pin++) 
+    {
+      digitalWrite(Pin, letter[m]);
+      m=m+1;
+    }
+    delayMicroseconds(timer);
+    // delay(timer);
+  }
+}
 
-void WriteLetter3Width(boolean letter[]){
+void WriteLetter3Width(boolean letter[])
+{
   m=0;
-  for (int n=0; n<=2; n++){
-  for (int Pin = 2; Pin <=6; Pin++) {
-    digitalWrite(Pin, letter[m]);
-    m=m+1;}
-    delayMicroseconds(timer);}}
+  for (int n=0; n<=2; n++)
+  {
+    for (int Pin = 2; Pin <=6; Pin++)
+    {
+      digitalWrite(Pin, letter[m]);
+      m=m+1;
+    }
+    delayMicroseconds(timer);
+    // delay(timer);
+  }
+}
 
- void WriteLetter2Width(boolean letter[]){
+void WriteLetter2Width(boolean letter[])
+{
   m=0;
-  for (int n=0; n<=1; n++){
-  for (int Pin = 2; Pin <=6; Pin++) {
-    digitalWrite(Pin, letter[m]);
-    m=m+1;}
-    delayMicroseconds(timer);}}
+  for (int n=0; n<=1; n++)
+  {
+    for (int Pin = 2; Pin <=6; Pin++)
+    {
+      digitalWrite(Pin, letter[m]);
+      m=m+1;
+    }
+    delayMicroseconds(timer);
+    // delay(timer);
+  }
+}
  
- void WriteLetter1Width(boolean letter[]){
+void WriteLetter1Width(boolean letter[])
+{
   m=0;
-   for (int Pin = 2; Pin <=6; Pin++) {
+  for (int Pin = 2; Pin <=6; Pin++) 
+  {
     digitalWrite(Pin, space[m]);
-    m=m+1;}
-    delayMicroseconds(timer);}
+    m=m+1;
+  }
+  delayMicroseconds(timer);
+  // delay(timer);
+}
   
